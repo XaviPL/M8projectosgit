4acf6be95d794e26721ba8e64e3ec760376112e4
XaviPL <93482.joan23@fje.edu>
2016-09-28 17:19:59 +0200

    version2java

--- Fichero.java
+++ Fichero.java
@@ -1,7 +1,7 @@
     public class Fichero {
      
     	public static void main(String[] args) {		
-    		System.out.println("Hola Mundo");
+    		System.out.println("Hola Menes");
     	}
      
     }
